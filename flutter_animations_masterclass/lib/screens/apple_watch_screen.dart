b21deabd92ff76f65305019580d3c44cc656aa20
chanhooop <cksghwwkd@gmail.com>
2023-07-11 19:33:39 +0900

    ing

--- flutter_animations_masterclass/lib/screens/apple_watch_screen.dart
+++ flutter_animations_masterclass/lib/screens/apple_watch_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class AppleWatchScreen extends StatefulWidget {
@@ -30,20 +32,40 @@ class _AppleWatchScreenState extends State<AppleWatchScreen> {
 class AppleWatchPaintr extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+
     Paint redCirclePaint = Paint()
       ..color = Colors.redAccent.shade200.withOpacity(0.5)
       ..strokeWidth = 25
       ..strokeJoin = StrokeJoin.bevel
+      ..style = PaintingStyle.stroke;
+    Paint redArcPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 25
+      ..strokeJoin = StrokeJoin.bevel
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
     Paint blueCirclePaint = Paint()
       ..color = Colors.blueAccent.shade200.withOpacity(0.5)
       ..strokeWidth = 25
       ..strokeJoin = StrokeJoin.bevel
       ..style = PaintingStyle.stroke;
+    Paint blueArcPaint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 25
+      ..strokeJoin = StrokeJoin.bevel
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
     Paint greenCirclePaint = Paint()
       ..color = Colors.greenAccent.shade200.withOpacity(0.5)
       ..strokeWidth = 25
       ..strokeJoin = StrokeJoin.bevel
+      ..style = PaintingStyle.stroke;
+    Paint greenArcPaint = Paint()
+      ..color = Colors.green
+      ..strokeWidth = 25
+      ..strokeJoin = StrokeJoin.bevel
+      ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
     canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.width / 2,
         redCirclePaint);
@@ -51,6 +73,10 @@ class AppleWatchPaintr extends CustomPainter {
         (size.width / 2) * 0.8, blueCirclePaint);
     canvas.drawCircle(Offset(size.width / 2, size.height / 2),
         (size.width / 2) * 0.6, greenCirclePaint);
+
+    canvas.drawArc(rect, 3 * pi / 2, pi, false, redArcPaint);
+    canvas.drawArc(rect, 3 * pi / 2, pi, false, blueArcPaint);
+    canvas.drawArc(rect, 3 * pi / 2, pi, false, greenArcPaint);
   }
 
   @override
