4a22b0b39c32d2750c3f25d34c6360e923b272cb
chanhooop <cksghwwkd@gmail.com>
2023-07-12 16:51:52 +0900

    apple watch

--- flutter_animations_masterclass/lib/screens/apple_watch_screen.dart
+++ flutter_animations_masterclass/lib/screens/apple_watch_screen.dart
@@ -9,7 +9,39 @@ class AppleWatchScreen extends StatefulWidget {
   State<AppleWatchScreen> createState() => _AppleWatchScreenState();
 }
 
-class _AppleWatchScreenState extends State<AppleWatchScreen> {
+class _AppleWatchScreenState extends State<AppleWatchScreen>
+    with SingleTickerProviderStateMixin {
+  final random = Random();
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: Duration(milliseconds: 2000),
+  )..forward();
+
+  late CurvedAnimation _curve =
+      CurvedAnimation(parent: _animationController, curve: Curves.bounceInOut);
+  final redEndNum = Random().nextDouble() * 2;
+  late Animation<double> _redProgress;
+  late Animation<double> _blueProgress;
+  late Animation<double> _greenProgress;
+
+  @override
+  void initState() {
+    super.initState();
+    final redEndNum = random.nextDouble() * 2;
+    final blueEndNum = random.nextDouble() * 2;
+    final greenEndNum = random.nextDouble() * 2;
+    _redProgress = Tween(begin: 0.005, end: redEndNum).animate(_curve);
+    _blueProgress = Tween(begin: 0.005, end: blueEndNum).animate(_curve);
+    _greenProgress = Tween(begin: 0.005, end: greenEndNum).animate(_curve);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _animationController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,19 +52,64 @@ class _AppleWatchScreenState extends State<AppleWatchScreen> {
         title: Text('Apple Watch'),
       ),
       body: Center(
-        child: CustomPaint(
-          painter: AppleWatchPaintr(),
-          size: Size(300, 300),
+        child: AnimatedBuilder(
+          builder: (context, snapshot) {
+            return CustomPaint(
+              painter: AppleWatchPaintr(progress: [
+                _redProgress.value,
+                _blueProgress.value,
+                _greenProgress.value
+              ]),
+              size: Size(300, 300),
+            );
+          },
+          animation: _curve,
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _animateValues();
+        },
+        child: Icon(Icons.refresh),
+      ),
     );
+  }
+
+  void _animateValues() {
+    final newRedBegin = _redProgress.value;
+    final newBlueBegin = _blueProgress.value;
+    final newGreenBegin = _greenProgress.value;
+    final double newRedEnd = random.nextDouble() * 2;
+    final double newBlueEnd = random.nextDouble() * 2;
+    final double newGreenEnd = random.nextDouble() * 2;
+
+    setState(() {
+      _redProgress = Tween(begin: newRedBegin, end: newRedEnd).animate(_curve);
+      _blueProgress =
+          Tween(begin: newBlueBegin, end: newBlueEnd).animate(_curve);
+      _greenProgress =
+          Tween(begin: newGreenBegin, end: newGreenEnd).animate(_curve);
+    });
+    _animationController.forward(from: 0);
   }
 }
 
 class AppleWatchPaintr extends CustomPainter {
+  final List<double> progress;
+  AppleWatchPaintr({required this.progress});
+
   @override
   void paint(Canvas canvas, Size size) {
-    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double redCircleRadius = size.width / 2;
+    double blueCircleRadius = (size.width / 2) * 0.8;
+    double greenCircleRadius = (size.width / 2) * 0.6;
+
+    Rect redArcRect = Rect.fromCircle(center: center, radius: redCircleRadius);
+    Rect blueArcRect =
+        Rect.fromCircle(center: center, radius: blueCircleRadius);
+    Rect greenArcRect =
+        Rect.fromCircle(center: center, radius: greenCircleRadius);
 
     Paint redCirclePaint = Paint()
       ..color = Colors.redAccent.shade200.withOpacity(0.5)
@@ -67,16 +144,19 @@ class AppleWatchPaintr extends CustomPainter {
       ..strokeJoin = StrokeJoin.bevel
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.stroke;
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2), size.width / 2,
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), redCircleRadius,
         redCirclePaint);
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), blueCircleRadius,
+        blueCirclePaint);
     canvas.drawCircle(Offset(size.width / 2, size.height / 2),
-        (size.width / 2) * 0.8, blueCirclePaint);
-    canvas.drawCircle(Offset(size.width / 2, size.height / 2),
-        (size.width / 2) * 0.6, greenCirclePaint);
+        greenCircleRadius, greenCirclePaint);
 
-    canvas.drawArc(rect, 3 * pi / 2, pi, false, redArcPaint);
-    canvas.drawArc(rect, 3 * pi / 2, pi, false, blueArcPaint);
-    canvas.drawArc(rect, 3 * pi / 2, pi, false, greenArcPaint);
+    canvas.drawArc(
+        redArcRect, 3 * pi / 2, progress[0] * pi, false, redArcPaint);
+    canvas.drawArc(
+        blueArcRect, 3 * pi / 2, progress[1] * pi, false, blueArcPaint);
+    canvas.drawArc(
+        greenArcRect, 3 * pi / 2, progress[2] * pi, false, greenArcPaint);
   }
 
   @override
