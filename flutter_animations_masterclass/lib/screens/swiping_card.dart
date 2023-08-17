4580fdb853d003d493de22ea075f00abefdb387b
chanhooop <cksghwwkd@gmail.com>
2023-08-17 21:00:14 +0900

    ing

--- flutter_animations_masterclass/lib/screens/swiping_card.dart
+++ flutter_animations_masterclass/lib/screens/swiping_card.dart
@@ -40,11 +40,14 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
     final dropZone = size.width + 100;
 
     if (_position.value.abs() >= bound) {
-      if (_position.value.isNegative) {
-        _position.animateTo(dropZone * -1);
-      } else {
-        _position.animateTo(dropZone);
-      }
+      final factor = _position.value.isNegative ? -1 : 1;
+      // whenComplete = 에니메이션이 끝난후 작동
+      _position.animateTo(dropZone * factor).whenComplete(() {
+        _position.value = 0;
+        setState(() {
+          _index = _index == 4 ? 1 : _index + 1;
+        });
+      });
     } else {
       _position.animateTo(0, curve: Curves.easeOut);
     }
@@ -57,64 +60,82 @@ class _SwipingCardScreenState extends State<SwipingCardScreen>
     super.dispose();
   }
 
+  // 카드 인덱스
+  int _index = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Swiping Card'),
       ),
-      body: AnimatedBuilder(
-        animation: _position,
-        builder: (context, child) {
-          final angle = _rotation
-              .transform((_position.value + size.width / 2) / size.width);
-          print(angle);
-          final scale = _scale.transform(_position.value.abs() / size.width);
-          return Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Positioned(
-                top: 100,
-                child: Transform.scale(
-                  scale: scale,
-                  child: Material(
-                    elevation: 10,
-                    color: Colors.blue.shade100,
-                    child: SizedBox(
-                      width: size.width * 0.8,
-                      height: size.height * 0.5,
+      body: Column(
+        children: [
+          /// 카드
+          SizedBox(
+            height: 500,
+            child: AnimatedBuilder(
+              animation: _position,
+              builder: (context, child) {
+                // 카드 넘어가는 각도
+                final angle = _rotation
+                    .transform((_position.value + size.width / 2) / size.width);
+
+                // 뒷부분 카드 스케일
+                // min함수 최솟값을 정해서 최소값이상으로 값이 상승시 최소값 표시
+                final scale = _scale
+                    .transform(min(_position.value.abs() / size.width, 1.0));
+
+                return Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      top: 100,
+                      child: Transform.scale(
+                          scale: scale,
+                          child: Card(index: _index == 4 ? 1 : _index + 1)),
                     ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 100,
-                child: GestureDetector(
-                  onHorizontalDragUpdate: (details) {
-                    _onHorizontalDragUpdate(details);
-                  },
-                  onHorizontalDragEnd: (details) {
-                    _onHorizontalDragEnd();
-                  },
-                  child: Transform.translate(
-                    offset: Offset(_position.value, 0.0),
-                    child: Transform.rotate(
-                      angle: angle * (pi / 180),
-                      child: Material(
-                        elevation: 10,
-                        color: Colors.red.shade100,
-                        child: SizedBox(
-                          width: size.width * 0.8,
-                          height: size.height * 0.5,
+                    Positioned(
+                      top: 100,
+                      child: GestureDetector(
+                        onHorizontalDragUpdate: (details) {
+                          _onHorizontalDragUpdate(details);
+                        },
+                        onHorizontalDragEnd: (details) {
+                          _onHorizontalDragEnd();
+                        },
+                        child: Transform.translate(
+                          offset: Offset(_position.value, 0.0),
+                          child: Transform.rotate(
+                              angle: angle * (pi / 180),
+                              child: Card(index: _index)),
                         ),
                       ),
                     ),
+                  ],
+                );
+              },
+            ),
+          ),
+
+          /// 버튼
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(50),
                   ),
-                ),
-              ),
-            ],
-          );
-        },
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -132,6 +153,10 @@ class Card extends StatelessWidget {
       child: SizedBox(
         width: size.width * 0.8,
         height: size.height * 0.5,
+        child: Image.asset(
+          'assets/covers/${index}.jpeg',
+          fit: BoxFit.fill,
+        ),
       ),
     );
   }
