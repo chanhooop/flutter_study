43b1e66c4896b7d58f1088b5a535d7edbeb46a0b
chanhooop <cksghwwkd@gmail.com>
2024-08-15 20:33:05 +0900

    riverpod ing

--- serverless_example/lib/app/service/middle_ware/dio_config.dart
+++ serverless_example/lib/app/service/middle_ware/dio_config.dart
@@ -7,7 +7,7 @@ import 'custom_interceptor.dart';
 class DioConfig {
   /// 일반 http
   Dio dio = Dio(BaseOptions(
-    baseUrl: 'https://api-jbhd.thesports.io', // 개발서버
+    baseUrl: 'https://api.jbfc.kr', // 전북서버
     // headers: {
     //   'user-Agent': '${USER_AGENT} gmembers(${PACKAGE_INFO.version}(${PACKAGE_INFO.buildNumber}) / ${OS})'
     // },
