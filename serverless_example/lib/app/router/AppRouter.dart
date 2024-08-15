e5900e77fab10ae19dc807794c128205b684b9e0
chanhooop <cksghwwkd@gmail.com>
2024-08-15 20:28:38 +0900

    riverpod ing

--- serverless_example/lib/app/router/AppRouter.dart
+++ serverless_example/lib/app/router/AppRouter.dart
@@ -1,6 +1,4 @@
 
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:serverless_example/app/view/test_view.dart';
